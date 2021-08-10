e214ffb0e6e4478b0ba9054b8a192275678b8936
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-08-10 10:54:27 +0800

    fix draft left in 11 index.ts

--- src/11-TupleToObject-easy/index.ts
+++ src/11-TupleToObject-easy/index.ts
@@ -11,6 +11,3 @@ const result: TupleToObject<typeof tuple> = {
   "model 3": "model 3",
   "model X": "model X",
 };
-
-let a = "foo"; // type of a is *string*
-let b = "bar" as const; // type of a is *"bar"*
